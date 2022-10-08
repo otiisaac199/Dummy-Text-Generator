f5aacfb1fef5e6f9904430347a90ad831826ceb2
otiisaac199 <otichinazaisaac19@gmail.com>
2022-10-08 13:33:18 +0100

    features: added the handleSubmit function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello world");
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi,
+          rerum.
+        </p>
+      </article>
+    </section>
+  );
 }
 
 export default App;
